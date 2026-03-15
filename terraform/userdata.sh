10ccf1d145a40230959e7cce2afd9c8503179b14
mariepcc <maria.pacocha358@gmail.com>
2026-03-15 22:34:09 +0100

    add amazon-ecr-credential-helper

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 apt-get update -y
 apt-get upgrade -y
-
-apt-get install -y docker.io awscli
+apt-get install -y docker.io awscli amazon-ecr-credential-helper
 
 systemctl start docker
 systemctl enable docker
 
 usermod -aG docker ubuntu
 
-docker system prune -f
+mkdir -p /home/ubuntu/.docker
+echo '{"credsStore": "ecr-login"}' > /home/ubuntu/.docker/config.json
+chown -R ubuntu:ubuntu /home/ubuntu/.docker
