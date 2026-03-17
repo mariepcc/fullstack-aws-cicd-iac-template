af0f5d022ed73ae3c96ad8c275a9e542c8c791cb
mariepcc <maria.pacocha358@gmail.com>
2026-03-17 13:12:38 +0100

    install newer version of docker-compose

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y docker.io docker-compose awscli amazon-ecr-credential-helper
+apt-get install -y docker.io awscli amazon-ecr-credential-helper
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 systemctl start docker
 systemctl enable docker
